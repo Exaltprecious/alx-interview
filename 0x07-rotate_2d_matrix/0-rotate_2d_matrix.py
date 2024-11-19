cf94ecaa22b450e82e297bd2486e3a17c034f758
Exaltprecious <peculiarchristj1234@gmail.com>
2024-11-19 23:26:55 +0100

    Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""2D matrix rotation module. 
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates an m by n 2D matrix in place.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    co, ro = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if ro == -1:
+            ro = rows - 1
+            co += 1
+        matrix[-1].append(matrix[ro][co])
+        if co == cols - 1 and ro >= -1:
+            matrix.pop(ro)
+        ro -= 1
